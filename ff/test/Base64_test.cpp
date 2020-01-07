4ef68f2e8627b5852ec4c2eb3bac44a330f5e1fc
zhangjianxiong <you@example.com>
2020-01-07 03:49:35 -0800

    update

---
+++ ff/test/Base64_test.cpp
@@ -0,0 +1,98 @@
+/*
+ * Base64_test.cpp
+ *
+ *  Created on: Aug 2, 2019
+ *      Author: root
+ */
+
+
+#include <gtest/gtest.h>
+#include <ff/Base64.h>
+#include <ff/Tick.h>
+#include "TestDef.h"
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+using namespace NS_FF;
+
+TEST(Base64Test, Base64Test){
+	string str = "test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64"
+			"test string for base64";
+
+	int i = 0;
+	Tick tick;
+	tick.start();
+	while (++i < 1000) {
+		auto strEnc = Base64::Encrypt(str);
+		//cout << strEnc << endl;
+		stringstream ostr;
+		Base64::Decrypt(ostr, strEnc);
+		//cout << ostr.str() << endl;
+		EXPECT_EQ(str, ostr.str());
+	}
+	cout << "tick: " << tick.count() << endl;
+}
