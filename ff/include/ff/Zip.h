73fda388e4ba3bb9c897fa752ed6a1586f10543b
wdwarf <langkes@163.com>
2021-06-16 23:38:45 +0800

    update

--- ff/include/ff/Zip.h
+++ ff/include/ff/Zip.h
@@ -10,39 +10,41 @@
 
 #include <ff/Object.h>
 #include <ff/ZipEntry.h>
+#include <ff/Exception.h>
 #include <string>
 
 NS_FF_BEG
 
-class Zip {
+class Zip
+{
 public:
-	Zip(const std::string& filePath);
+	Zip(const std::string &filePath);
 	virtual ~Zip();
 
-	void createNew();
-	void open();
+	void createNew() _throws(Exception);
+	void open() _throws(Exception);
 	void close();
 	bool isOpened() const;
 
 	// 设定当前entry
-	Zip& operator <<(const ZipEntry& entry);
+	Zip &operator<<(const ZipEntry &entry) _throws(Exception);
 	/**
 	 * 添加一个文件file到压缩包
 	 * 文件会按原名称加入到当前entry，同zip(file);
 	 */
-	Zip& operator <<(const std::string& file);
+	Zip &operator<<(const std::string &file) _throws(Exception);
 
 	/**
 	 * 将源文件/目录src以名称newFileName加入到entry里去
 	 */
-	Zip& zip(const std::string& src, const std::string& entry = "",
-			const std::string& newFileName = "");
+	Zip &zip(const std::string &src, const std::string &entry = "",
+			 const std::string &newFileName = "") _throws(Exception);
 
-	const std::string& getFilePath() const;
+	const std::string &getFilePath() const;
 
 private:
 	class ZipImpl;
-	ZipImpl* impl;
+	ZipImpl *impl;
 };
 
 NS_FF_END
