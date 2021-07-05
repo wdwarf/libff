d83568ed160dda57e208b853cfe4f7b3d45bc681
wdwarf <langkes@163.com>
2021-07-05 16:35:14 +0800

    update

--- ff/include/ff/Curl.h
+++ ff/include/ff/Curl.h
@@ -10,17 +10,26 @@
 
 NS_FF_BEG
 
-using Headers = std::map<std::string, std::string>;
+using CurlHeaders = std::map<std::string, std::string>;
+using CurlParams = std::map<std::string, std::string>;
 
 class Curl {
  public:
   Curl();
+  Curl(const std::string& url, const CurlHeaders& headers = {});
   virtual ~Curl();
 
+  operator CURL*();
+
   Curl& setUrl(const std::string& url);
-  Curl& setHeaders(const Headers& headers);
+  Curl& setHeaders(const CurlHeaders& headers);
+  Curl& setOAuth2Token(const std::string& token);
+  Curl& setConnectTimeout(int64_t msTimeout);
+  Curl& setReadTimeout(int64_t msTimeout);
+  Curl& reset();
   std::stringstream& getOutputStream();
-  virtual Curl& perform();
+  Curl& setOutputStream(std::ostream* outputStream);
+  virtual CURLcode perform();
 
   template <typename... Args>
   Curl& setOption(CURLoption opt, Args... args) {
@@ -28,13 +37,53 @@ class Curl {
     return *this;
   }
 
- private:
+  template <class... Arg>
+  CURLcode getInfo(CURLINFO info, Arg... args) const {
+    return curl_easy_getinfo(m_curl, info, std::forward<Arg>(args)...);
+  }
+
+ protected:
   CURL* m_curl;
-  curl_slist* m_header;
+  curl_slist* m_headers;
   std::stringstream m_outputStream;
+  std::ostream* m_pOutputStream;
 
   static size_t CurlWriteData(void* buf, size_t size, size_t nmemb,
                               void* stream);
+};
+
+using HttpGet = Curl;
+
+class HttpPost : public Curl {
+ public:
+  HttpPost();
+  HttpPost(const std::string& url, const CurlParams& formDatas,
+           const CurlHeaders& headers = {});
+  virtual ~HttpPost();
+
+  HttpPost& setFormDatas(const CurlParams& datas);
+  HttpPost& setFormFile(const std::string& fieldName,
+                        const std::string& filePath);
+  CURLcode perform() override;
+
+ protected:
+  curl_httppost* m_httppost;
+  curl_httppost* m_httppostLast;
+};
+
+class HttpPut : public Curl {
+ public:
+  HttpPut& setContentType(const std::string& contentType);
+  HttpPut& setBody(const std::string& body);
+  CURLcode perform() override;
+
+ private:
+  std::string m_body;
+};
+
+class httpDelete : public Curl {
+ public:
+  CURLcode perform() override;
 };
 
 NS_FF_END
