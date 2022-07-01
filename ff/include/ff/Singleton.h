c5c4cc93e0587f8b683f2519e3f60870f5116e80
李亚武 <langke@live.cn>
2022-07-01 18:33:10 +0800

    update

--- ff/include/ff/Singleton.h
+++ ff/include/ff/Singleton.h
@@ -12,62 +12,66 @@
 
 NS_FF_BEG
 
-/*
- * 这个单例会在第一次调用时初始化
- * */
-template<class T>
-class Singleton: public T {
-public:
-	static T* getInstance() {
-		static Singleton<T> instance;
-		return &instance;
-	}
+#define SINGLETON_DEF(className)      \
+ public:                                \
+  static className* getInstance() {     \
+    static className instance;          \
+    return &instance;                   \
+  }                                     \
+                                        \
+ private:                               \
+  ~className() = default;               \
+  className() = default;                \
+  className(const className&) = delete; \
+  className& operator=(const className&) = delete;
 
-private:
-	~Singleton() {
-	}
-	Singleton() {
-	}
+/**
+ * @brief 这个单例会在第一次调用时初始化
+ */
+template <class T>
+class Singleton : public T {
+ public:
+  static T* getInstance() {
+    static Singleton<T> instance;
+    return &instance;
+  }
 
-	Singleton(const Singleton&);
-	Singleton& operator=(const Singleton&);
+ private:
+  ~Singleton() {}
+  Singleton() {}
 
+  Singleton(const Singleton&);
+  Singleton& operator=(const Singleton&);
 };
 
-/*
- * 这个单例会在main函数执行之前初始化
- * */
-template<class T>
-class Singleton2: public T {
-private:
-	struct object_creator {
-		object_creator() {
-			Singleton2<T>::getInstance();
-		}
-		inline void doNothing() const {
-		}
-	};
-	static object_creator creator_object;
+/**
+ * @brief 这个单例会在main函数执行之前初始化
+ */
+template <class T>
+class Singleton2 : public T {
+ private:
+  struct object_creator {
+    object_creator() { Singleton2<T>::getInstance(); }
+    inline void doNothing() const {}
+  };
+  static object_creator creator_object;
 
-public:
-	static T* getInstance() {
-		static Singleton2<T> instance;
-		creator_object.doNothing();
-		return &instance;
-	}
+ public:
+  static T* getInstance() {
+    static Singleton2<T> instance;
+    creator_object.doNothing();
+    return &instance;
+  }
 
-private:
-	~Singleton2() {
-	}
-	Singleton2() {
-	}
+ private:
+  ~Singleton2() {}
+  Singleton2() {}
 
-	Singleton2(const Singleton2&);
-	Singleton2& operator=(const Singleton2&);
-
+  Singleton2(const Singleton2&);
+  Singleton2& operator=(const Singleton2&);
 };
 
-template<class T>
+template <class T>
 typename Singleton2<T>::object_creator Singleton2<T>::creator_object;
 
 NS_FF_END
