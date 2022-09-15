de674703a3c34b74ac3dd3c3ded280755dfa7169
李亚武 <langke@live.cn>
2022-09-15 16:38:49 +0800

    update

--- ff/include/ff/EPoll.h
+++ ff/include/ff/EPoll.h
@@ -10,81 +10,84 @@
 
 #ifndef _WIN32
 
-#include <functional>
-#include <mutex>
-#include <map>
-#include <list>
-#include <vector>
-#include <thread>
-#include <poll.h>
 #include <ff/ff_config.h>
+#include <poll.h>
 #include <sys/epoll.h>
+
+#include <functional>
+#include <list>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <vector>
 
 NS_FF_BEG
 
 typedef std::function<void(int, int)> FdUpdateFunc;
 
 class LIBFF_API EPoll {
-public:
-	EPoll(uint32_t maxEpollEvents = 1000);
-	virtual ~EPoll();
+ public:
+  EPoll(uint32_t maxEpollEvents = 1000);
+  virtual ~EPoll();
 
-	bool addFd(int fd, const FdUpdateFunc& updateFunc);
-	bool delFd(int fd);
-	bool addEvents(int fd, int events);
-	bool delEvents(int fd, int events);
-	void update(int pollTimeout);
+  bool addFd(int fd, const FdUpdateFunc& updateFunc);
+  bool delFd(int fd);
+  bool addEvents(int fd, int events);
+  bool delEvents(int fd, int events);
+  void update(int pollTimeout);
 
-	void signal();
-protected:
-	int m_epFd;
-	int m_signalPipe[2];
-	std::vector<epoll_event> epollEvents;
-	// bool m_fdChanged;
-	struct FdInfo {
-		FdUpdateFunc updateFunc;
-		int fd;
-		int events;
-	};
-	typedef std::map<int, FdInfo> FdInfos;
-	FdInfos m_fdInfos;
+  void signal();
 
-	std::mutex m_fdInfosMutex;
-	std::mutex m_signalMutex;
+ protected:
+  int m_epFd;
+  int m_signalPipe[2];
+  std::vector<epoll_event> epollEvents;
+  // bool m_fdChanged;
+  struct FdInfo {
+    FdUpdateFunc updateFunc;
+    int fd;
+    int events;
+  };
+  typedef std::map<int, FdInfo> FdInfos;
+  FdInfos m_fdInfos;
 
-	bool initSignalPipe();
-	void uninitSignalPipe();
+  std::mutex m_fdInfosMutex;
+  std::mutex m_signalMutex;
 
-	bool addFd2Poll(int fd);
-	bool delFdFromPoll(int fd);
-	bool setEvent2Fd(int fd, int events);
+  bool initSignalPipe();
+  void uninitSignalPipe();
 
-	void onPipeEvents(int fd, int events);
-	bool doPoll(std::vector<pollfd>& ofds, int timeout);
+  bool addFd2Poll(int fd);
+  bool delFdFromPoll(int fd);
+  bool setEvent2Fd(int fd, int events);
+
+  void onPipeEvents(int fd, int events);
+  bool doPoll(std::vector<pollfd>& ofds, int timeout);
 };
 
 class LIBFF_API PollMgr {
-public:
-	PollMgr();
-	~PollMgr();
+ public:
+  PollMgr();
+  ~PollMgr();
 
-	static PollMgr& instance() {
-		static PollMgr g_pollMgr;
-		static std::once_flag onceFlag;
-		std::call_once(onceFlag, [&] {g_pollMgr.start();});
-		return g_pollMgr;
-	}
+  static PollMgr& instance() {
+    static PollMgr g_pollMgr;
+    static std::once_flag onceFlag;
+    std::call_once(onceFlag, [&] { g_pollMgr.start(); });
+    return g_pollMgr;
+  }
 
-	void start();
-	void stop();
+  void start();
+  void stop();
 
-	EPoll& getEPoll();
+  EPoll& getEPoll();
 
-private:
-	EPoll m_ep;
-	bool m_stoped;
-	std::thread m_pollThread;
-	void pollThreadFunc();
+ private:
+  EPoll m_ep;
+  bool m_stoped;
+  std::thread m_pollThread;
+  void pollThreadFunc();
 };
 
 using EPollPtr = std::shared_ptr<EPoll>;
