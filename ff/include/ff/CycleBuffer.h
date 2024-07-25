dc014be85da13350b9ab26d5e9650022db6d870a
liyawu <liyawu@zlg.cn>
2024-07-25 21:18:15 +0800

    add CycleBuffer

---
+++ ff/include/ff/CycleBuffer.h
@@ -0,0 +1,85 @@
+/**
+ * @file CycleBuffer.h
+ * @author liyawu
+ * @date 2024-07-23 11:21:36
+ * @description
+ */
+
+#ifndef _CYCLEBUFFER_H
+#define _CYCLEBUFFER_H
+
+#include <ff/ff_config.h>
+
+#include <vector>
+
+NS_FF_BEG
+
+template <typename T>
+class CycleBuffer {
+ public:
+  using SizeType = uint32_t;
+  CycleBuffer(SizeType capacity) {
+    m_capacity = capacity;
+    m_size = 0;
+    m_pos = 0;
+    m_buf = new T[capacity];
+  }
+
+  ~CycleBuffer() {
+    if (m_buf) delete[] m_buf;
+  }
+
+  SizeType size() const { return m_size; }
+
+  bool isFull() const { return m_size == m_capacity; }
+
+  bool isEmpty() const { return m_size <= 0; }
+
+  bool put(T o) {
+    if (isFull()) return false;
+    auto tailPos = tail();
+    m_buf[tailPos] = o;
+    ++m_size;
+    return true;
+  }
+
+  bool get(T& o) {
+    if (isEmpty()) return false;
+    o = m_buf[m_pos];
+
+    m_pos = (m_pos + 1) % m_capacity;
+    --m_size;
+    return true;
+  }
+
+  SizeType read(T* o, SizeType size) {
+    if (isEmpty()) return 0;
+    auto size2Read = std::min(size, m_size);
+
+    for (uint32_t i = 0; i < size2Read; ++i) {
+      auto tmpPos = (m_pos + i) % m_capacity;
+      o[i] = m_buf[tmpPos];
+    }
+
+    m_pos = (m_pos + size2Read) % m_capacity;
+    m_size -= size2Read;
+
+    return size2Read;
+  }
+
+  SizeType pos() const { return m_pos; }
+
+ private:
+  SizeType m_capacity = 0;
+  SizeType m_size = 0;
+  SizeType m_pos = 0;
+  T* m_buf = nullptr;
+  SizeType tail() const { return (m_pos + m_size) % m_capacity; }
+};
+
+using CharCycleBuffer = CycleBuffer<char>;
+using ByteCycleBuffer = CycleBuffer<uint8_t>;
+
+NS_FF_END
+
+#endif  // _CYCLEBUFFER_H
---
+++ ff/test/CycleBuffer_test.cpp
@@ -0,0 +1,100 @@
+/*
+ * Buffer_test.cpp
+ *
+ *  Created on: Aug 2, 2019
+ *      Author: root
+ */
+
+#include <ff/Buffer.h>
+#include <ff/CycleBuffer.h>
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "TestDef.h"
+
+using namespace std;
+
+USE_NS_FF
+
+TEST(CycleBufferTest, CycleBufferTest) {
+  CycleBuffer<uint8_t> buf(8);
+  ASSERT_EQ(buf.size(), 0);
+
+  buf.put(1);
+  ASSERT_EQ(buf.size(), 1);
+  LOGD << "size: " << buf.size();
+  uint8_t val = 0;
+  buf.get(val);
+  LOGD << "val: " << (uint16_t)val;
+  ASSERT_EQ(val, 1);
+  ASSERT_EQ(buf.size(), 0);
+  LOGD << "size: " << buf.size();
+  LOGD << "pos: " << buf.pos();
+
+  LOGD << "==============================";
+
+  buf.put(1);
+  buf.put(2);
+  buf.put(3);
+  buf.put(4);
+  buf.put(5);
+  buf.put(6);
+  LOGD << "size: " << buf.size();
+  LOGD << "pos: " << buf.pos();
+  LOGD << "==============================";
+
+  uint8_t arr[16]{0};
+  LOGD << buf.read(arr, 3);
+  LOGD << Buffer(arr, 3).toHexString();
+  LOGD << "size: " << buf.size();
+  LOGD << "pos: " << buf.pos();
+  LOGD << "==============================";
+
+  for (uint8_t i = 10; i > 0; --i) buf.put(i);
+  LOGD << "size: " << buf.size();
+  LOGD << "pos: " << buf.pos();
+  auto n = buf.read(arr, 30);
+  LOGD << n;
+  LOGD << Buffer(arr, n).toHexString();
+  LOGD << "size: " << buf.size();
+  LOGD << "pos: " << buf.pos();
+}
+
+TEST(CycleBufferTest, CycleBufferTest_int) {
+  CycleBuffer<int32_t> buf(8);
+  ASSERT_EQ(buf.size(), 0);
+  buf.put(-1);
+  LOGD << "size: " << buf.size();
+  LOGD << "pos: " << buf.pos();
+  int val = 0;
+  buf.get(val);
+  LOGD << "val: " << val;
+	ASSERT_EQ(val, -1);
+}
+
+TEST(CycleBufferTest, CycleBufferTest_double) {
+  CycleBuffer<double> buf(8);
+  ASSERT_EQ(buf.size(), 0);
+  buf.put(3.14159);
+  LOGD << "size: " << buf.size();
+  LOGD << "pos: " << buf.pos();
+  double val = 0;
+  buf.get(val);
+  LOGD << "val: " << val;
+	ASSERT_EQ(val, 3.14159);
+}
+
+TEST(CycleBufferTest, CycleBufferTest_str) {
+  CycleBuffer<string> buf(8);
+  ASSERT_EQ(buf.size(), 0);
+  buf.put("str value");
+  LOGD << "size: " << buf.size();
+  LOGD << "pos: " << buf.pos();
+  string val;
+  buf.get(val);
+  LOGD << "val: " << val;
+	ASSERT_EQ(val, "str value");
+}
